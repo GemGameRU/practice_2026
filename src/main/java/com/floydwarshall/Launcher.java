624c4d7dacee3c1f4c3318769e8c7c0f4aa7e172
GemGameRU <gemgameru@mail.ru>
2026-07-13 12:58:48 +0300

    Add fatJar (jar with dependencies)

---
+++ src/main/java/com/floydwarshall/Launcher.java
@@ -0,0 +1,8 @@
+package com.floydwarshall;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // This calls the main method inside your App class
+        App.main(args);
+    }
+}
